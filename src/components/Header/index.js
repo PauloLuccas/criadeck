ac71db36d2f3a501d24edaaf6b5cdc308700cea1
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2021-05-27 14:50:50 -0300

    Estruturando projeto

---
+++ src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './styles.css';
+
+function Header() {
+  return (
+    <header className={styles.container}>
+        <h1>Header funcionando..</h1>
+    </header>
+  );
+}
+
+export default Header;
