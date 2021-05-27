72dd0acfaa8dbf7396f20c921fbd6b6a50ca49ba
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2021-05-27 16:46:07 -0300

    Header finalizado

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import './styles.css';
 
-import styles from './styles.css';
+import btnMenu from '../../assets/img/menu_hamburger.png';
 
 function Header() {
   return (
-    <header className={styles.container}>
-        <h1>Header funcionando..</h1>
+    <header>
+        <div className="container">
+          <a href="#" className="logo"></a>
+
+          <nav>
+            <ul>
+              <li><a href="#">Pixel</a></li>
+              <li><a href="#">Mega</a></li>
+              <li><a href="#">Arena</a></li>
+              <li><a href="#">Ultra</a></li>
+            </ul>
+          </nav>
+
+          <div className="menu-mobile">
+            <nav>
+              <ul>
+                <li><a href="#">Pixel</a></li>
+                <li><a href="#">Mega</a></li>
+                <li><a href="#">Arena</a></li>
+                <li><a href="#">Ultra</a></li>
+              </ul>
+            </nav>
+          </div>
+
+          <input type="submit" value="Orçamento" />
+
+          <a id="js-open-menu" className="btn-menu">
+            <img src={btnMenu} alt="Btn Menu" />
+          </a>
+
+        </div>
     </header>
   );
 }
