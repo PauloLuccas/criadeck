f1db4568d8e7207c5593d6d48e2f98d35382deb4
PAULO LUCCAS <paulo_lucas_raimundo@hotmail.com>
2021-05-29 13:40:48 -0300

    ajustes no styles e adicionando a biblioteca AOS

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Routes from './Routes';
+import AOS from 'aos';
+
+AOS.init();
 
 ReactDOM.render(
   <React.StrictMode>
